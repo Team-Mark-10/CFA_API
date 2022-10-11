3b0d8b606f4d04ef83f7920e9334a3efa432e4ca
Your Name <indegamedev@gmail.com>
2022-10-11 22:30:39 +1100

    connects to db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use mongodb::{bson::doc, options::ClientOptions, Client};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let mut connection_string: Option<String> = None;
+
+    for (key, value) in env::vars() {
+        if key == "CONNECTION_STRING" {
+            connection_string = Some(value);
+        }
+    }
+
+    match connection_string {
+        None => { println!("No connection string"); return (); },
+        Some(s) =>  { connect_mongodb(s).await; return (); }
+    }
+}
+
+async fn connect_mongodb(connection_string: String) -> mongodb::error::Result<()> {
+    let mut client_options = ClientOptions::parse(connection_string).await?;
+
+    client_options.app_name = Some("CFA HUD".to_string());
+
+    let client = Client::with_options(client_options)?;
+
+    client
+        .database("cfa-hud")
+        .run_command(doc! {"ping" : 1}, None)
+        .await?;
+
+    println!("Connected successfully.");
+
+    for db_name in client.list_database_names(None, None).await? {
+        println!("{}", db_name);
+    }
+
+    let db = client.database("cfa-hud");
+
+    for collection_name in db.list_collection_names(None).await? {
+        println!("{}", collection_name)
+    }
+
+    Ok(())
 }
