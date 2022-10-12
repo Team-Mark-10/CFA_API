4a7c0257072517be9cec1aaa7d11426167f9fb9f
Aditha Doratiyawa <indegamedev@gmail.com>
2022-10-12 16:43:04 +1100

    adding readings working

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use std::env;
+use serde::{Serialize, Deserialize};
+use serde_json::{Value, json};
 use dotenv::dotenv;
-use mongodb::{bson::doc, options::ClientOptions, Client};
+use mongodb::{bson::{doc, serde_helpers::bson_datetime_as_rfc3339_string}, options::ClientOptions, Client};
+use futures::stream::StreamExt;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer};
+use chrono;
+
+const QUERY_LIMIT : i32 = 50;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -31,7 +37,11 @@ async fn main() -> std::io::Result<()> {
             HttpServer::new(
                 move || {App::new()
                     .app_data(web::Data::new(c.clone()))
-                    .service(ping)
+                    .service(get_status)
+                    .service(get_patient)
+                    .service(post_patient)
+                    .service(get_readings)
+                    .service(post_readings)
                 })
                 .bind(("127.0.0.1", 8080))?
                 .run()
@@ -42,9 +52,111 @@ async fn main() -> std::io::Result<()> {
 
 
 #[get("/status")]
-async fn ping(client: web::Data<Client>) -> HttpResponse {
+async fn get_status(client: web::Data<Client>) -> HttpResponse {
     HttpResponse::Ok().body("Hi")
 
+}
+
+#[get("/patient/{bid}")]
+async fn get_patient(client: web::Data<Client>) -> HttpResponse {
+    HttpResponse::Ok().body("Hi")
+
+}
+
+#[post("/patient")]
+async fn post_patient(client: web::Data<Client>, patient: web::Json<Patient>) -> HttpResponse {
+    HttpResponse::Ok().body("Hi")
+
+}
+#[derive(Serialize, Deserialize)]
+struct ReadingsQueryParam{
+    from: Option<String>,
+    until: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct DBReading{
+    reading_at: bson::DateTime, 
+    data: Vec<ContinuousData>,
+    created_at: bson::DateTime,
+}
+
+
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct NewReading{
+
+    #[serde(with = "bson_datetime_as_rfc3339_string")]
+    reading_at: bson::DateTime,
+    data: Vec<ContinuousData>,
+}
+#[derive(Serialize, Deserialize)]
+struct GetReadingsResponse {
+    readings: Vec<DBReading>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct ContinuousData {
+    service_id: String,
+    alias: String,
+    value: f32,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Patient {
+    bid : String,
+    alias: String,
+    data: Vec<Value>,
+}
+
+
+#[get("/readings")]
+async fn get_readings(client: web::Data<Client>, query: web::Query<ReadingsQueryParam>) -> HttpResponse {
+    let readings_collection =  client.database("cfa-hud").collection("readings");
+
+    let cursor = readings_collection.find(None, None).await;
+
+    match cursor {
+        Err(e) => HttpResponse::InternalServerError().body("Couldn't get readings"),
+        Ok(mut c) => {
+            let mut readings = Vec::<DBReading>::new();
+            while let Some(result) = c.next().await {
+                    match result {
+                        Ok(doc) => readings.push(doc),
+                        _ => return HttpResponse::InternalServerError().body("Couldn't get readings"),
+                    }
+            }
+
+            HttpResponse::Ok().json(web::Json( GetReadingsResponse {readings: readings}) )
+        }
+    }
+}
+
+#[post("/readings")]
+async fn post_readings(client: web::Data<Client>, reading: web::Json<NewReading>) -> HttpResponse {
+    let readings_collection =  client.database("cfa-hud").collection::<DBReading>("readings");
+
+    println!("{}", reading.reading_at);
+   
+    let new_reading = convert_to_db_reading(reading);
+
+    println!("{} {}", new_reading.reading_at, new_reading.created_at);
+    let result = readings_collection.insert_one(new_reading, None).await;
+
+    match result {
+        Ok(r) => HttpResponse::Ok().body("Hi"),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+    }
+}
+
+fn convert_to_db_reading(reading: web::Json<NewReading>) -> DBReading {
+    let cloned = reading.clone();
+
+    DBReading {
+        reading_at: cloned.reading_at, 
+        data: cloned.data,
+        created_at: bson::DateTime::now(),
+    }
 }
 
 async fn connect_mongodb(connection_string: String) -> mongodb::error::Result<Client> {
