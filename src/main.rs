dd9ca476c13e5e584f9da1a0ec6da99955f53aad
Your Name <indegamedev@gmail.com>
2022-10-17 00:46:15 +1100

    opened server to all ips

--- src/main.rs
+++ src/main.rs
@@ -238,7 +238,7 @@ async fn main() -> std::io::Result<()> {
                     .service(get_readings)
                     .service(post_readings)
             })
-            .bind(("127.0.0.1", 8080))?
+            .bind(("0.0.0.0", 8080))?
             .run()
             .await
         }
