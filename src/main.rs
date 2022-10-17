b31a19e63bc469bc40cf64398d58a0f84f1a1e09
Your Name <indegamedev@gmail.com>
2022-10-18 01:08:17 +1100

    cleaned code

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
-use std::{future::{ready, Ready}, env, rc::Rc, sync::Arc, ops::Deref};
-use futures_util::future::LocalBoxFuture;
 use actix_web::{
-    ResponseError, get, post, web, App, HttpResponse, HttpServer,  Error, middleware::Condition,
-    dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform}
+    dev::ServiceRequest,
+    get,
+    post, web, App, HttpResponse, HttpServer, ResponseError,
 };
-use derive_more::{Display};
-use actix_web_httpauth::{extractors::{AuthenticationError, basic::{self,BasicAuth, Config}}, middleware::HttpAuthentication};
-use actix_http::header::{self, HeaderMap, HeaderValue};
+use actix_web_httpauth::{
+    extractors::basic::BasicAuth,
+    middleware::HttpAuthentication,
+};
+use derive_more::Display;
 use dotenv::dotenv;
 use futures::stream::StreamExt;
+
+use log::{info, warn, error, debug};
 use mongodb::{
     bson::{doc, serde_helpers::bson_datetime_as_rfc3339_string},
     options::ClientOptions,
     Client,
 };
-use log::{debug, warn, info};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use std::{
+    env,
+};
 
 // The amount of readings returned per page.
 const PAGE_SIZE: usize = 50;
-
 
 // A reading from the database.
 #[derive(Serialize, Deserialize, Clone)]
@@ -104,7 +108,6 @@ async fn get_readings(
     // Adds filter for readings after the from date. from string must be in URL Encoded RFC3339
     // format.
     if let Some(from) = &query.from {
-        println!("{}", from);
         if let Ok(date) = bson::DateTime::parse_rfc3339_str(from) {
             filter_options.insert("reading_at", doc!("$gte": date));
         } else {
@@ -115,7 +118,6 @@ async fn get_readings(
     // Adds filter for readings before the until date. until string must be in URL Encoded RFC3339
     // format.
     if let Some(until) = &query.until {
-        println!("{}", until);
         if let Ok(date) = bson::DateTime::parse_rfc3339_str(until) {
             filter_options.insert("reading_at", doc!("$lt": date));
         } else {
@@ -163,12 +165,19 @@ struct PostReadingsPayload {
 // An API endpoint to add readings to the database. Readings have to be in a specific format given
 // by the NewReading struct.
 #[post("/readings")]
-async fn post_readings(client: web::Data<Client>, payload: web::Json<PostReadingsPayload>) -> HttpResponse {
+async fn post_readings(
+    client: web::Data<Client>,
+    payload: web::Json<PostReadingsPayload>,
+) -> HttpResponse {
     let readings_collection = client
         .database("cfa-hud")
         .collection::<DBReading>("readings");
 
-    let new_readings = payload.readings.iter().map(|x| convert_to_db_reading(x)).collect::<Vec<_>>();
+    let new_readings = payload
+        .readings
+        .iter()
+        .map(|x| convert_to_db_reading(x))
+        .collect::<Vec<_>>();
 
     let result = readings_collection.insert_many(new_readings, None).await;
 
@@ -193,7 +202,7 @@ fn convert_to_db_reading(reading: &NewReading) -> DBReading {
 // Connects to the MongoDB database and returns a client handle if successful.
 async fn connect_mongodb(connection_string: String) -> mongodb::error::Result<Client> {
     let mut client_options = ClientOptions::parse(connection_string).await?;
- 
+
     client_options.app_name = Some("CFA HUD".to_string());
 
     let client = Client::with_options(client_options)?;
@@ -203,7 +212,7 @@ async fn connect_mongodb(connection_string: String) -> mongodb::error::Result<Cl
         .run_command(doc! {"ping" : 1}, None)
         .await?;
 
-    println!("Connected successfully.");
+    info!("Connected to DB successfully.");
     Ok(client)
 }
 
@@ -215,21 +224,22 @@ async fn validator(
 ) -> Result<ServiceRequest, (actix_web::Error, ServiceRequest)> {
     match username.is_some() && password.is_some() {
         true => {
-           let authorised = match username.unwrap() == credentials.user_id() {
-                true => {
-                    match credentials.password() {
-                        Some(pwd) => pwd == password.unwrap(),
-                        None => false,
-                    }
-                }
-                false => false
-           };
+            let authorised = match username.unwrap() == credentials.user_id() {
+                true => match credentials.password() {
+                    Some(pwd) => pwd == password.unwrap(),
+                    None => false,
+                },
+                false => false,
+            };
 
-           match authorised {
+            match authorised {
                 true => Ok(req),
-                false => Err((actix_web::error::ErrorUnauthorized(AuthError::CredentialsInvalid), req)),
-           }
-        },
+                false => Err((
+                    actix_web::error::ErrorUnauthorized(AuthError::CredentialsInvalid),
+                    req,
+                )),
+            }
+        }
         false => Ok(req),
     }
 }
@@ -243,7 +253,9 @@ enum AuthError {
 impl ResponseError for AuthError {
     fn error_response(&self) -> HttpResponse {
         match self {
-            AuthError::CredentialsInvalid => HttpResponse::Unauthorized().json("{\"error\": \"Invalid Credentials\"}"),
+            AuthError::CredentialsInvalid => {
+                HttpResponse::Unauthorized().json("{\"error\": \"Invalid Credentials\"}")
+            }
         }
     }
 }
@@ -255,18 +267,14 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     let username = match env::var("USERAME") {
-        Err(_) => {
-            None
-        }
+        Err(_) => None,
         Ok(username) => {
             info!("Username logged");
             Some(username)
         }
     };
     let password = match env::var("PASSWORD") {
-        Err(_) => {
-            None
-        }
+        Err(_) => None,
         Ok(pwd) => {
             info!("Password logged.");
             Some(pwd)
@@ -282,57 +290,39 @@ async fn main() -> std::io::Result<()> {
     // Loads the connection string from the environment variables.
     let client = match env::var("CONNECTION_STRING") {
         Err(_) => {
-            println!("No connection string");
+            error!("No connection string");
             None
         }
         Ok(s) => match connect_mongodb(s).await {
             Err(_) => {
-                println!("Couldn't complete database connection test.");
+                error!("Couldn't complete database connection test.");
                 None
             }
             Ok(c) => {
-                println!("Completed database connection test.");
+                debug!("Completed database connection test.");
                 Some(c)
             }
         },
     };
-    
+
+    let auth_closure = |username: Option<String>, password: Option<String>| move |req: ServiceRequest, credentials: BasicAuth| validator(req, credentials, username.clone(), password.clone());
     // Starts the webserver if the app successfully connected to the DB.
+    let current_auth_closure = auth_closure(username, password);
+
     match client {
         None => {
-            println!("No client.");
+            error!("No client could be established.");
             Ok(())
         }
         Some(c) => {
-           HttpServer::new(move || {
-            let username = match env::var("USERAME") {
-                Err(_) => {
-                    None
-                }
-                Ok(username) => {
-                    Some(username)
-                }
-           };
-           let password = match env::var("PASSWORD") {
-                Err(_) => {
-                    None
-                }
-                Ok(pwd) => {
-                    Some(pwd)
-                }
-           };
-
-            
-             let closure = move |req: ServiceRequest, credentials: BasicAuth| { validator(req, credentials, username.clone(), password.clone() ) };
-
-             App::new()
-                .wrap(HttpAuthentication::basic(closure))
-                .app_data(web::Data::new(c.clone()))
-                .service(get_status)
-                .service(get_readings)
-                .service(post_readings)
-
-           })
+            HttpServer::new(move || {
+                App::new()
+                    .wrap(HttpAuthentication::basic(current_auth_closure.clone()))
+                    .app_data(web::Data::new(c.clone()))
+                    .service(get_status)
+                    .service(get_readings)
+                    .service(post_readings)
+            })
             .bind(("0.0.0.0", 8080))?
             .run()
             .await
